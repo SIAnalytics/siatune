273f4194f444472dec47974ba7257886c8e761a6
Younghwan Na <yhna@Younghwanui-MacBookPro.local>
2022-05-12 14:08:07 +0900

    Fix minor bug

--- mmtune/mm/tasks/builder.py
+++ mmtune/mm/tasks/builder.py
@@ -1,9 +1,7 @@
 from mmcv.utils import Registry
 
-from .base import BaseTask
-
 TASKS = Registry('tasks')
 
 
-def build_task_processor(task_name: str) -> BaseTask:
+def build_task_processor(task_name: str):
     return TASKS.build(dict(type=task_name))
