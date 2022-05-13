e6334c09bed046268a067036b599cde82bafaa97
Younghwan Na <100389977+yhna940@users.noreply.github.com>
2022-05-13 09:17:03 +0900

    Fix mmseg tune cfg file (#10)
    Co-authored-by: Younghwan Na <yhna@Younghwanui-MacBookPro.local>

--- configs/mmtune/mmseg_asynchb_nevergrad_pso.py
+++ configs/mmtune/mmseg_asynchb_nevergrad_pso.py
@@ -1,13 +1,14 @@
 _base_ = [
-    '../_base_/context/train.py', '../_base_/searcher/nevergrad_pso.py',
-    '../_base_/scheduler/asynchb.py', '../_base_/space/mmseg_model.py',
-    '../_base_/space/optimizer.py'
+    './_base_/context/train.py', './_base_/searcher/nevergrad_pso.py',
+    './_base_/scheduler/asynchb.py', './_base_/space/mmseg_model.py',
+    './_base_/space/optimizer.py', './_base_/space/batch_size.py'
 ]
 
-space = dict(
-    model={{_base_.model}},
-    optimizer={{_base_.optimizer}},
-)
+space = {
+    'model': {{_base_.model}},
+    'optimizer': {{_base_.optimizer}},
+    'data.samples_per_gpus': {{_base_.batch_size}}
+}
 
 metric = 'val/mIoU'
 mode = 'max'
