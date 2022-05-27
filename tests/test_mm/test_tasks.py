f9a21b2ef6871c1dd128ece3a5907397545b4ff4
nijkah <nijkah@gmail.com>
2022-05-27 17:39:58 +0900

    fix ci

--- tests/test_mm/test_tasks.py
+++ tests/test_mm/test_tasks.py
@@ -12,13 +12,13 @@ def test_mmseg(mock_build_dataset, mock_train_model):
 
     parser = argparse.ArgumentParser()
     config_path = 'configs/mmseg/pspnet/pspnet_r18-d8_4x4_512x512_80k_potsdam.py'  # noqa
-    parser.add_argument('--config')
+    parser.add_argument('config')
+    args = parser.parse_args([config_path])
 
     task = MMSegmentation()
-    parser = task.add_arguments(parser)
-    args = parser.parse_args(['--config', config_path])
+    task.set_args(args)
 
-    task.run(args=args)
+    task.run()
 
 
 @patch.object(MMDetection, 'train_model', return_value=None)
@@ -28,13 +28,13 @@ def test_mmdet(mock_build_dataset, mock_train_model):
 
     parser = argparse.ArgumentParser()
     config_path = 'configs/mmdet/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
-    parser.add_argument('--config')
+    parser.add_argument('config')
+    args = parser.parse_args([config_path])
 
     task = MMDetection()
-    parser = task.add_arguments(parser)
-    args = parser.parse_args(['--config', config_path])
+    task.set_args(args)
 
-    task.run(args=args)
+    task.run()
 
 
 @patch.object(MMClassification, 'train_model', return_value=None)
@@ -44,22 +44,22 @@ def test_mmcls(mock_build_dataset, mock_train_model):
 
     parser = argparse.ArgumentParser()
     config_path = 'configs/mmcls/resnet/resnet18_8xb16_cifar10.py'
-    parser.add_argument('--config')
+    parser.add_argument('config')
+    args = parser.parse_args([config_path])
 
     task = MMClassification()
-    parser = task.add_arguments(parser)
-    args = parser.parse_args(['--config', config_path])
+    task.set_args(args)
 
-    task.run(args=args)
+    task.run()
 
 
 def test_sphere():
     parser = argparse.ArgumentParser()
     config_path = 'configs/mmtune/bbo_sphere_nevergrad_oneplusone.py'
-    parser.add_argument('--config')
+    parser.add_argument('config')
+    args = parser.parse_args([config_path])
 
     task = Sphere()
-    parser = task.add_arguments(parser)
-    args = parser.parse_args(['--config', config_path])
+    task.set_args(args)
 
-    task.run(args=args)
+    task.run()
