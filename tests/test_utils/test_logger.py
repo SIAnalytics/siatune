2a1f321fa6475dfd8c8c1e5226461f88612def0d
Younghwan Na <100389977+yhna940@users.noreply.github.com>
2022-06-09 10:45:20 +0900

    Fix log file test

--- tests/test_utils/test_logger.py
+++ tests/test_utils/test_logger.py
@@ -1,11 +1,13 @@
+import tempfile
 from os import path as osp
 
 from mmtune.utils import get_root_logger
 
 
 def test_get_root_logger():
-
-    logger = get_root_logger(log_file='test.log')
-    logger.info('test')
-    assert osp.isfile('test.log')
-    assert logger.name == 'mmtune'
+    with tempfile.TemporaryDirectory() as tmpdir:
+        save_path = osp.join(tmpdir, 'test.log')
+        logger = get_root_logger(log_file=save_path)
+        logger.info('test')
+        assert osp.exists(save_path)
+        assert logger.name == 'mmtune'
