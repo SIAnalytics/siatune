fffb5415966abc870cc7983a2fca260c6ad03a5f
KKIEEK <ethan9867@gmail.com>
2022-09-05 15:09:48 +0900

    Add test code

--- mmtune/ray/searchers/__init__.py
+++ mmtune/ray/searchers/__init__.py
@@ -1,7 +1,11 @@
+from .ax import AxSearch
 from .builder import SEARCHERS, build_searcher
+from .flaml import BlendSearch, CFOSearch
+from .hyperopt import HyperOptSearch
 from .nevergrad import NevergradSearch
 from .trust_region import TrustRegionSearcher
 
 __all__ = [
-    'SEARCHERS', 'build_searcher', 'NevergradSearch', 'TrustRegionSearcher'
+    'SEARCHERS', 'build_searcher', 'AxSearch', 'BlendSearch', 'CFOSearch',
+    'HyperOptSearch', 'NevergradSearch', 'TrustRegionSearcher'
 ]
--- tests/test_ray/test_searchers.py
+++ tests/test_ray/test_searchers.py
@@ -1,6 +1,8 @@
+import pytest
 from ray import tune
 
-from mmtune.ray.searchers import (SEARCHERS, NevergradSearch,
+from mmtune.ray.searchers import (SEARCHERS, AxSearch, BlendSearch, CFOSearch,
+                                  HyperOptSearch, NevergradSearch,
                                   TrustRegionSearcher, build_searcher)
 
 
@@ -13,49 +15,74 @@ def test_build_searcher():
     assert isinstance(build_searcher({'type': 'TestSearcher'}), TestSearcher)
 
 
-def test_nevergradsearch():
-    budget = 2
-    config = {
-        'steps': 10,
-        'width': tune.uniform(0, 20),
-        'height': tune.uniform(-100, 100),
-    }
+@pytest.fixture
+def config():
+    return dict(
+        steps=10, width=tune.uniform(0, 20), height=tune.uniform(-100, 100))
 
-    def _objective(config):
-        width, height = config['width'], config['height']
-        for step in range(config['steps']):
-            intermediate_score = (0.1 +
-                                  width * step / 100)**(-1) + height * 0.1
-            tune.report(iterations=step, mean_loss=intermediate_score)
 
+def trainable(config):
+    width, height = config['width'], config['height']
+    for step in range(config['steps']):
+        intermediate_score = (0.1 + width * step / 100)**(-1) + height * 0.1
+        tune.report(iterations=step, mean_loss=intermediate_score)
+
+
+def test_ax(config):
     tune.run(
-        _objective,
+        trainable,
         metric='mean_loss',
         mode='min',
-        search_alg=NevergradSearch(optimizer='PSO', budget=budget),
-        num_samples=budget,
+        search_alg=AxSearch(),
+        num_samples=2,
         config=config)
 
 
-def test_trust_region_searcher():
-    budget = 2
-    config = {
-        'steps': 10,
-        'width': tune.uniform(0, 20),
-        'height': tune.uniform(-100, 100),
-    }
-
-    def _objective(config):
-        width, height = config['width'], config['height']
-        for step in range(config['steps']):
-            intermediate_score = (0.1 +
-                                  width * step / 100)**(-1) + height * 0.1
-            tune.report(iterations=step, mean_loss=intermediate_score)
-
+def test_blend(config):
     tune.run(
-        _objective,
+        trainable,
+        metric='mean_loss',
+        mode='min',
+        search_alg=BlendSearch(),
+        num_samples=2,
+        config=config)
+
+
+def test_cfo(config):
+    tune.run(
+        trainable,
+        metric='mean_loss',
+        mode='min',
+        search_alg=CFOSearch(),
+        num_samples=2,
+        config=config)
+
+
+def test_hyperopt(config):
+    tune.run(
+        trainable,
+        metric='mean_loss',
+        mode='min',
+        search_alg=HyperOptSearch(),
+        num_samples=2,
+        config=config)
+
+
+def test_nevergrad(config):
+    tune.run(
+        trainable,
+        metric='mean_loss',
+        mode='min',
+        search_alg=NevergradSearch(optimizer='PSO', budget=2),
+        num_samples=2,
+        config=config)
+
+
+def test_trust_region(config):
+    tune.run(
+        trainable,
         metric='mean_loss',
         mode='min',
         search_alg=TrustRegionSearcher(),
-        num_samples=budget,
+        num_samples=2,
         config=config)
