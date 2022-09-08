99a1b51fffedce9c43e7ab47d3cfcec4129cb139
nijkah <nijkah@gmail.com>
2022-09-08 15:32:18 +0900

    Add original copyright

--- mmtune/mm/tasks/cont_test_func.py
+++ mmtune/mm/tasks/cont_test_func.py
@@ -1,5 +1,10 @@
 # Copyright (c) SI-Analytics. All rights reserved.
-# https://github.com/facebookresearch/nevergrad/blob/main/nevergrad/functions/corefuncs.py
+#
+# Brought from https://github.com/facebookresearch/nevergrad/blob/main/nevergrad/functions/corefuncs.py # noqa E501
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 import argparse
 from math import exp, sqrt, tanh
 
