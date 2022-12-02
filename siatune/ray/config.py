06a8dea531649728340ed2901ef33f8f3ee08f45
KKIEEK <kkieek@KKIEEKui-MacBookPro.local>
2022-12-03 03:33:54 +0900

    Fix typo

--- siatune/ray/config.py
+++ siatune/ray/config.py
@@ -26,7 +26,7 @@ class CustomBackendConfig(BackendConfig):
         return _CustomTorchBackend
 
 
-def _set_nccl_network_interface() -> str:
+def _set_nccl_network_interface():
     """Set the appropriate NCCL network interface to use."""
 
     if 'NCCL_SOCKET_IFNAME' not in os.environ:
@@ -45,30 +45,30 @@ class _CustomTorchBackend(_TorchBackend):
 
     def on_start(self, worker_group: WorkerGroup,
                  backend_config: BackendConfig):
-        if dist.is_available():
-            if 'NCCL_SOCKET_IFNAME' in os.environ:
-                worker_group.execute(_set_nccl_network_interface)
-
-            master_addr, master_port = worker_group.execute_single(
-                0, get_address_and_port)
-
-            def set_env_vars(addr, port, rank, world_size):
-                os.environ['MASTER_ADDR'] = addr
-                os.environ['MASTER_PORT'] = str(port)
-                os.environ['RANK'] = str(rank)
-                os.environ['WORLD_SIZE'] = str(world_size)
-
-            setup_futures = []
-            for i in range(len(worker_group)):
-                setup_futures.append(
-                    worker_group.execute_single_async(
-                        i,
-                        set_env_vars,
-                        addr=master_addr,
-                        port=master_port,
-                        rank=i,
-                        world_size=len(worker_group),
-                    ))
-            ray.get(setup_futures)
-        else:
+        if not dist.is_available():
             raise RuntimeError('Distributed torch is not available.')
+
+        worker_group.execute(_set_nccl_network_interface)
+
+        master_addr, master_port = worker_group.execute_single(
+            0, get_address_and_port)
+
+        def set_env_vars(addr, port, rank, world_size):
+            os.environ['MASTER_ADDR'] = addr
+            os.environ['MASTER_PORT'] = str(port)
+            os.environ['RANK'] = str(rank)
+            os.environ['LOCAL_RANK'] = str(rank)
+            os.environ['WORLD_SIZE'] = str(world_size)
+
+        setup_futures = []
+        for i in range(len(worker_group)):
+            setup_futures.append(
+                worker_group.execute_single_async(
+                    i,
+                    set_env_vars,
+                    addr=master_addr,
+                    port=master_port,
+                    rank=i,
+                    world_size=len(worker_group),
+                ))
+        ray.get(setup_futures)
