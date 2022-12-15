1db82a84203acafb51a2507dccc450ede8ae2064
KKIEEK <kkieek@KKIEEKui-MacBookPro.local>
2022-12-15 13:48:58 +0900

    Update mmseg

--- siatune/mm/tasks/mmseg.py
+++ siatune/mm/tasks/mmseg.py
@@ -3,14 +3,9 @@ import argparse
 import copy
 import os
 import time
+import warnings
 from os import path as osp
-from typing import Optional, Sequence
-
-import mmcv
-import torch
-import torch.distributed as dist
-from mmcv.runner import get_dist_info
-from mmcv.utils import Config, DictAction, get_git_hash
+from typing import Sequence
 
 from .builder import TASKS
 from .mmtrainbase import MMTrainBasedTask
@@ -18,16 +13,18 @@ from .mmtrainbase import MMTrainBasedTask
 
 @TASKS.register_module()
 class MMSegmentation(MMTrainBasedTask):
-    """MMSegmentation Wrapping class for ray tune."""
+    """MMSegmentation wrapper class for `ray.tune`.
 
-    def parse_args(self, args: Sequence[str]) -> argparse.Namespace:
-        """Define and parse the necessary arguments for the task.
+    It is modified from https://github.com/open-mmlab/mmsegmentation/blob/v0.25.0/tools/train.py
 
-        Args:
-            args (Sequence[str]): The args.
-        Returns:
-            argparse.Namespace: The parsed args.
-        """
+    Attributes:
+        args (Sequence[str]):
+    """
+
+    VERSION = 'v0.25.0'
+
+    def parse_args(task_args: Sequence[str]):
+        from mmcv.utils import DictAction
 
         parser = argparse.ArgumentParser(description='Train a segmentor')
         parser.add_argument('config', help='train config file path')
@@ -41,6 +38,24 @@ class MMSegmentation(MMTrainBasedTask):
             '--no-validate',
             action='store_true',
             help='whether not to evaluate the checkpoint during training')
+        group_gpus = parser.add_mutually_exclusive_group()
+        group_gpus.add_argument(
+            '--gpus',
+            type=int,
+            help='(Deprecated, please use --gpu-id) number of gpus to use '
+            '(only applicable to non-distributed training)')
+        group_gpus.add_argument(
+            '--gpu-ids',
+            type=int,
+            nargs='+',
+            help='(Deprecated, please use --gpu-id) ids of gpus to use '
+            '(only applicable to non-distributed training)')
+        group_gpus.add_argument(
+            '--gpu-id',
+            type=int,
+            default=0,
+            help='id of gpu to use '
+            '(only applicable to non-distributed training)')
         parser.add_argument(
             '--seed', type=int, default=None, help='random seed')
         parser.add_argument(
@@ -52,100 +67,74 @@ class MMSegmentation(MMTrainBasedTask):
             action='store_true',
             help='whether to set deterministic options for CUDNN backend.')
         parser.add_argument(
+            '--options',
+            nargs='+',
+            action=DictAction,
+            help=
+            "--options is deprecated in favor of --cfg_options' and it will "
+            'not be supported in version v0.22.0. Override some settings in the '
+            'used config, the key-value pair in xxx=yyy format will be merged '
+            'into config file. If the value to be overwritten is a list, it '
+            'should be like key="[a,b]" or key=a,b It also allows nested '
+            'list/tuple values, e.g. key="[(a,b),(c,d)]" Note that the quotation '
+            'marks are necessary and that no white space is allowed.')
+        parser.add_argument(
             '--cfg-options',
             nargs='+',
             action=DictAction,
-            help='override some settings in the used config, the key-value '
-            'pair in xxx=yyy format will be merged into config file. If the '
-            'value to be overwritten is a list, it should be like key="[a,b]" '
-            'or key=a,b It also allows nested list/tuple values, e.g. '
-            'key="[(a,b),(c,d)]" Note that the quotation marks are necessary '
-            'and that no white space is allowed.')
+            help='override some settings in the used config, the key-value pair '
+            'in xxx=yyy format will be merged into config file. If the value to '
+            'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+            'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+            'Note that the quotation marks are necessary and that no white space '
+            'is allowed.')
+        parser.add_argument(
+            '--launcher',
+            choices=['none', 'pytorch', 'slurm', 'mpi'],
+            default='none',
+            help='job launcher')
+        parser.add_argument('--local_rank', type=int, default=0)
         parser.add_argument(
             '--auto-resume',
             action='store_true',
             help='resume from the latest checkpoint automatically.')
-        args = parser.parse_args(args)
+        args = parser.parse_args(task_args)
+        if 'LOCAL_RANK' not in os.environ:
+            os.environ['LOCAL_RANK'] = str(args.local_rank)
+
+        if args.options and args.cfg_options:
+            raise ValueError(
+                '--options and --cfg-options cannot be both '
+                'specified, --options is deprecated in favor of --cfg-options. '
+                '--options will not be supported in version v0.22.0.')
+        if args.options:
+            warnings.warn(
+                '--options is deprecated in favor of --cfg-options. '
+                '--options will not be supported in version v0.22.0.')
+            args.cfg_options = args.options
+
         return args
 
-    def build_model(self,
-                    cfg: Config,
-                    train_cfg: Optional[Config] = None,
-                    test_cfg: Optional[Config] = None) -> torch.nn.Module:
-        """Build the model from configs.
-
-        Args:
-            cfg (Config): The configs.
-            train_cfg (Optional[Config]):
-                The train opt. Defaults to None.
-            test_cfg (Optional[Config]):
-                The Test opt. Defaults to None.
-        Returns:
-            torch.nn.Module: The model.
-        """
-
-        from mmseg.models.builder import build_segmentor
-        return build_segmentor(cfg, train_cfg, test_cfg)
-
-    def build_dataset(
-            self,
-            cfg: Config,
-            default_args: Optional[Config] = None) -> torch.utils.data.Dataset:
-        """Build the dataset from configs.
-
-        Args:
-            cfg (Config): The configs.
-            default_args (Optional[Config]):
-                The default args. Defaults to None.
-        Returns:
-            torch.utils.data.Dataset: The dataset.
-        """
-
-        from mmseg.datasets.builder import build_dataset
-        return build_dataset(cfg, default_args)
-
-    def train_model(self,
-                    model: torch.nn.Module,
-                    dataset: torch.utils.data.Dataset,
-                    cfg: Config,
-                    distributed: bool = True,
-                    validate: bool = False,
-                    timestamp: Optional[str] = None,
-                    meta: Optional[dict] = None) -> None:
-        """Train the model.
-
-        Args:
-            model (torch.nn.Module): The model.
-            dataset (torch.utils.data.Dataset): The dataset.
-            cfg (Config): The configs.
-            distributed (bool):
-                Whether or not distributed. Defaults to True.
-            validate (bool):
-                Whether or not validate. Defaults to False.
-            timestamp (Optional[str]):
-                The timestamp. Defaults to None.
-            meta (Optional[dict]): The meta. Defaults to None.
-        """
-
-        from mmseg.apis.train import train_segmentor
-        return train_segmentor(model, dataset, cfg, distributed, validate,
-                               timestamp, meta)
-
-    def run(self, *, args, **kwargs) -> None:
+    def run(self, args: argparse.Namespace):
         """Run the task.
 
         Args:
             args (argparse.Namespace):
                 The args that received from context manager.
         """
-
+        import mmcv
+        import torch
+        import torch.distributed as dist
+        from mmcv.cnn.utils import revert_sync_batchnorm
+        from mmcv.runner import get_dist_info, init_dist
+        from mmcv.utils import Config, get_git_hash
         from mmseg import __version__
-        from mmseg.apis import init_random_seed, set_random_seed
-        from mmseg.utils import (collect_env, get_root_logger,
+        from mmseg.apis import (init_random_seed, set_random_seed,
+                                train_segmentor)
+        from mmseg.datasets import build_dataset
+        from mmseg.models import build_segmentor
+        from mmseg.utils import (collect_env, get_device, get_root_logger,
                                  setup_multi_processes)
-
-        if 'LOCAL_RANK' not in os.environ:
-            os.environ['LOCAL_RANK'] = str(dist.get_rank())
 
         cfg = Config.fromfile(args.config)
         if args.cfg_options is not None:
@@ -155,8 +144,7 @@ class MMSegmentation(MMTrainBasedTask):
         if cfg.get('cudnn_benchmark', False):
             torch.backends.cudnn.benchmark = True
 
-        # work_dir is determined in this priority: CLI >
-        # segment in file > filename
+        # work_dir is determined in this priority: CLI > segment in file > filename
         if args.work_dir is not None:
             # update configs according to CLI args if args.work_dir is not None
             cfg.work_dir = args.work_dir
@@ -168,14 +156,31 @@ class MMSegmentation(MMTrainBasedTask):
             cfg.load_from = args.load_from
         if args.resume_from is not None:
             cfg.resume_from = args.resume_from
+        if args.gpus is not None:
+            cfg.gpu_ids = range(1)
+            warnings.warn('`--gpus` is deprecated because we only support '
+                          'single GPU mode in non-distributed training. '
+                          'Use `gpus=1` now.')
+        if args.gpu_ids is not None:
+            cfg.gpu_ids = args.gpu_ids[0:1]
+            warnings.warn('`--gpu-ids` is deprecated, please use `--gpu-id`. '
+                          'Because we only support single GPU mode in '
+                          'non-distributed training. Use the first GPU '
+                          'in `gpu_ids` now.')
+        if args.gpus is None and args.gpu_ids is None:
+            cfg.gpu_ids = [args.gpu_id]
 
         cfg.auto_resume = args.auto_resume
 
         # init distributed env first, since logger depends on the dist info.
-        distributed = True
-        # gpu_ids is used to calculate iter when resuming checkpoint
-        _, world_size = get_dist_info()
-        cfg.gpu_ids = range(world_size)
+        if args.launcher == 'none':
+            distributed = False
+        else:
+            distributed = True
+            init_dist(args.launcher, **cfg.dist_params)
+            # gpu_ids is used to calculate iter when resuming checkpoint
+            _, world_size = get_dist_info()
+            cfg.gpu_ids = range(world_size)
 
         # create work_dir
         mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
@@ -196,8 +201,8 @@ class MMSegmentation(MMTrainBasedTask):
         env_info_dict = collect_env()
         env_info = '\n'.join([f'{k}: {v}' for k, v in env_info_dict.items()])
         dash_line = '-' * 60 + '\n'
-        logger.info('Environment info:\n' + dash_line + env_info +  # noqa W504
-                    '\n' + dash_line)
+        logger.info('Environment info:\n' + dash_line + env_info + '\n' +
+                    dash_line)
         meta['env_info'] = env_info
 
         # log some basic info
@@ -205,7 +210,8 @@ class MMSegmentation(MMTrainBasedTask):
         logger.info(f'Config:\n{cfg.pretty_text}')
 
         # set random seeds
-        seed = init_random_seed(args.seed)
+        cfg.device = get_device()
+        seed = init_random_seed(args.seed, device=cfg.device)
         seed = seed + dist.get_rank() if args.diff_seed else seed
         logger.info(f'Set random seed to {seed}, '
                     f'deterministic: {args.deterministic}')
@@ -214,20 +220,27 @@ class MMSegmentation(MMTrainBasedTask):
         meta['seed'] = seed
         meta['exp_name'] = osp.basename(args.config)
 
-        model = self.build_model(
+        model = build_segmentor(
             cfg.model,
             train_cfg=cfg.get('train_cfg'),
             test_cfg=cfg.get('test_cfg'))
         model.init_weights()
 
         # SyncBN is not support for DP
+        if not distributed:
+            warnings.warn(
+                'SyncBN is only supported with DDP. To be compatible with DP, '
+                'we convert SyncBN to BN. Please use dist_train.sh which can '
+                'avoid this error.')
+            model = revert_sync_batchnorm(model)
+
         logger.info(model)
 
-        datasets = [self.build_dataset(cfg.data.train)]
+        datasets = [build_dataset(cfg.data.train)]
         if len(cfg.workflow) == 2:
             val_dataset = copy.deepcopy(cfg.data.val)
             val_dataset.pipeline = cfg.data.train.pipeline
-            datasets.append(self.build_dataset(val_dataset))
+            datasets.append(build_dataset(val_dataset))
         if cfg.checkpoint_config is not None:
             # save mmseg version, config file content and class names in
             # checkpoints as meta data
@@ -240,11 +253,11 @@ class MMSegmentation(MMTrainBasedTask):
         model.CLASSES = datasets[0].CLASSES
         # passing checkpoint meta for saving best checkpoint
         meta.update(cfg.checkpoint_config.meta)
-        self.train_model(
+        train_segmentor(
             model,
             datasets,
             cfg,
-            distributed=True,
+            distributed=distributed,
             validate=(not args.no_validate),
             timestamp=timestamp,
             meta=meta)
