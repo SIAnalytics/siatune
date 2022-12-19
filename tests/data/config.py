54dc8f1352410d67e3df8dd563e1b45197f8e4d2
Junhwa Song <ethan9867@gmail.com>
2022-12-19 12:19:41 +0900

    Support MMEditing task (#105)

--- tests/data/config.py
+++ tests/data/config.py
@@ -1,5 +1,8 @@
 data = dict(workers_per_gpu=1, train=dict())
 model = dict()
+train_cfg = None
+test_cfg = None
 checkpoint_config = dict(interval=-1)
 log_level = 'INFO'
+work_dir = ''
 workflow = [('train', 1)]
