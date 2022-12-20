85e7ce292af57243673250fc5776694857769602
Junhwa Song <ethan9867@gmail.com>
2022-12-20 15:28:00 +0900

    [Refactor] builder for search_alg and scheduler (#114)

--- siatune/tune/searchers/builder.py
+++ siatune/tune/searchers/builder.py
@@ -4,7 +4,10 @@ from mmcv.utils import Config, Registry
 from ray import tune
 from ray.tune.search import Searcher
 
-SEARCHERS = Registry('searchers')
+SEARCHERS = Registry('searcher')
+
+# Dynamically import search_alg
+# Refer to https://github.com/ray-project/ray/blob/master/python/ray/tune/search/__init__.py  # noqa
 for func in set(tune.search.SEARCH_ALG_IMPORT.values()):
     SEARCHERS.register_module(module=func())
 
