f9eb3a37404eb190eb007d21ada6da142db41bc8
KKIEEK <kkieek@KKIEEKui-MacBookPro.local>
2022-12-20 15:39:04 +0900

    Add config for MedianStoppingRule

---
+++ configs/_base_/scheduler/median.py
@@ -0,0 +1,9 @@
+# Refer to https://github.com/ray-project/ray/blob/master/python/ray/tune/schedulers/median_stopping_rule.py
+
+trial_scheduler = dict(
+    type='MedianStoppingRule',
+    time_attr='time_total_s',
+    grace_period=60,
+    min_samples_required=3,
+    min_time_slice=0,
+    hard_stop=True)
