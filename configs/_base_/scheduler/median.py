d749c9459235374074b50115d0c860ec4705469e
KKIEEK <kkieek@KKIEEKui-MacBookPro.local>
2022-12-20 16:02:07 +0900

    Add noqa

--- configs/_base_/scheduler/median.py
+++ configs/_base_/scheduler/median.py
@@ -1,4 +1,4 @@
-# Refer to https://github.com/ray-project/ray/blob/master/python/ray/tune/schedulers/median_stopping_rule.py
+# Refer to https://github.com/ray-project/ray/blob/master/python/ray/tune/schedulers/median_stopping_rule.py  # noqa
 
 trial_scheduler = dict(
     type='MedianStoppingRule',
