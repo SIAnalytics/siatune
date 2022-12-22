fab9e61925a96b83eb67c7797c23b9fc4e8bfcfc
KKIEEK <kkieek@KKIEEKui-MacBookPro.local>
2022-12-22 13:47:58 +0900

    Fix test code

--- tests/test_apis/test_apis.py
+++ tests/test_apis/test_apis.py
@@ -1,5 +1,5 @@
 import os
-import tempfile
+from tempfile import TemporaryDirectory
 from unittest.mock import MagicMock
 
 import mmcv
@@ -8,15 +8,12 @@ from siatune.apis import log_analysis
 
 
 def test_log_analysis():
-    mock_analysis = MagicMock()
+    mock_results = MagicMock()
 
-    task_config = mmcv.Config(dict(model=dict(type='TempModel')))
-
-    mock_analysis.best_config = task_config
-    mock_analysis.best_result = dict(accuracy=50)
-    mock_analysis.best_logdir = 'temp_log_dir'
-    mock_analysis.results = [dict(accuracy=50)]
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-        log_analysis(mock_analysis, tmpdir)
+    with TemporaryDirectory() as tmpdir:
+        with TemporaryDirectory() as logdir:
+            mock_results.get_best_result.return_value = MagicMock(
+                log_dir=logdir,
+                config=mmcv.Config(dict(model=dict(type='TestModel'))))
+            log_analysis(mock_results, log_dir=tmpdir)
         assert os.path.exists(os.path.join(tmpdir, 'best_trial'))
