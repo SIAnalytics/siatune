31c89821b4b423620cd1d3f67e47bc3916779142
Younghwan Na <100389977+yhna940@users.noreply.github.com>
2023-01-04 11:47:46 +0900

    :art: Apply lint

--- siatune/core/launch.py
+++ siatune/core/launch.py
@@ -3,7 +3,7 @@ from typing import Callable
 
 import ray
 import torch
-from ray.air.config import ScalingConfig
+from ray import tune
 from ray.train._internal.utils import get_address_and_port
 from ray.tune import with_resources as reserve_resources
 
@@ -13,37 +13,26 @@ from siatune.utils import set_env_vars
 class DistTorchLauncher:
 
     def __init__(self, num_cpus_per_worker: int = 1, num_workers: int = 1):
-
-        num_remote_worker: int
-        if num_workers > 1:
-            num_remote_worker = num_workers - 1
-        else:
-            num_remote_worker = num_workers
-        use_gpu: bool = torch.cuda.is_available()
-
-        self._resources = ScalingConfig(
-            trainer_resources=dict(CPU=num_cpus_per_worker, GPU=int(use_gpu)),
-            use_gpu=use_gpu,
-            num_workers=num_remote_worker,
-            resources_per_worker=dict(CPU=num_cpus_per_worker))
-        return
-
-    @property
-    def resources(self):
-        return self._resources
+        self.num_workers = num_workers
+        self.num_cpus_per_worker = num_cpus_per_worker
+        self.num_gpus_per_worker = int(torch.cuda.is_available())
+        self.resources = tune.PlacementGroupFactory([
+            dict(CPU=self.num_cpus_per_worker, GPU=self.num_gpus_per_worker),
+        ] * num_workers)
 
     def launch(self, func: Callable, *args, **kwargs):
-        num_workers = self.resources.num_workers + 1
-        num_cpus_per_worker = self.resources.resources_per_worker.get('CPU')
         addr, port = get_address_and_port()
 
         def job(rank):
-            set_env_vars(rank, num_workers, addr, port)
+            set_env_vars(rank, self.num_workers, addr, port)
             func(*args, **kwargs)
 
         remote_job = ray.remote(job).options(
-            num_cpus=num_cpus_per_worker, num_gpus=1)
-        futures = [remote_job.remote(rank) for rank in range(1, num_workers)]
+            num_cpus=self.num_cpus_per_worker,
+            num_gpus=self.num_gpus_per_worker)
+        futures = [
+            remote_job.remote(rank) for rank in range(1, self.num_workers)
+        ]
         job(rank=0)
         ray.get(futures)
         return
