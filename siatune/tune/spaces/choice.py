b7385cbc94f2c02affee42b0b23934e2adcc8277
Junhwa Song <ethan9867@gmail.com>
2023-01-18 11:36:39 +0900

    [Enhance] Add support for dict input in Choice (#138)
    * Add support for dict input in Choice
    
    * Fix typo
    
    * Apply lint
    
    * Update docs
    
    * Add test code
    
    * Fix
    
    Co-authored-by: KKIEEK <kkieek@KKIEEKui-MacBookPro.local>
    Co-authored-by: YH <100389977+yhna940@users.noreply.github.com>
    Co-authored-by: Hakjin Lee <nijkah@gmail.com>

--- siatune/tune/spaces/choice.py
+++ siatune/tune/spaces/choice.py
@@ -1,5 +1,5 @@
 # Copyright (c) SI-Analytics. All rights reserved.
-from typing import Callable, Optional, Sequence
+from typing import Callable, Optional, Sequence, Union
 
 import ray.tune as tune
 from ray.tune.search.sample import Domain
@@ -14,16 +14,19 @@ class Choice(BaseSpace):
     """Sample a categorical value.
 
     Args:
-        categories (Sequence): The categories.
-        alias (Sequence, optional): A alias to be expressed.
-            Defaults to None.
+        categories (Sequence | dict): The categorical search space to choose
+            one. If categories is dict, keys of dict will overwrite the alias.
+        alias (Sequence, optional): A alias to be expressed. Defaults to None.
     """
 
     sample: Callable = tune.choice
 
     def __init__(self,
-                 categories: Sequence,
+                 categories: Union[Sequence, dict],
                  alias: Optional[Sequence] = None) -> None:
+        if isinstance(categories, dict):
+            alias, categories = zip(*[(k, v) for k, v in categories.items()])
+
         if alias is not None:
             assert isinstance(alias, Sequence)
             assert len(categories) == len(alias)
